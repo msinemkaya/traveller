38a72870ac1ca998bb5d4536323bfe96a3702bef
Melike Sinem Kaya <msinemkaya6@gmail.com>
2024-01-11 16:55:40 +0300

    feat: added some basic routing layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import './App.css'
 import { Route, Routes } from 'react-router-dom'
 import { Layout } from './components/layout/Layout.jsx'
+import { Login } from './components/sections/Login.jsx'
+import { Register } from './components/sections/Register.jsx'
+import { NoPage } from './components/layout/NoPage.jsx'
 
 function App() {
 
@@ -9,6 +12,9 @@ function App() {
       <Route path='/' element={<Layout/>}>
 
       </Route>
+      <Route path='/login' element={<Login/>}/>
+      <Route path='/register' element={<Register/>}/>
+      <Route path='*' element={<NoPage/>}/>
     </Routes>
   )
 }
